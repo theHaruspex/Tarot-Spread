08401ec402ccb1bbba729346847e8cc394b6bbfb
Derious Vaughn <75146768+theHaruspex@users.noreply.github.com>
2022-12-30 21:18:02 +0000

    Initial commit

---
+++ card_spread.js
@@ -0,0 +1,11 @@
+// var w = window.innerWidth;
+// var h = window.innerHeight;
+
+
+// let element = document.getElementById("test");
+// element.style.background = "red"
+
+
+// let text_content = element.textContent;
+
+// element.textContent = "(w=" + w + ", h=" + h + ")"
