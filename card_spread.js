6247f8f08c78da56330856acf838ba4ca54f19a2
Derious Vaughn <75146768+theHaruspex@users.noreply.github.com>
2023-01-03 20:20:13 +0000

    Begun work on the card_spread page's transition timings.

--- card_spread.js
+++ card_spread.js
@@ -2,6 +2,11 @@ const past_section = document.getElementById("past_section")
 const present_section = document.getElementById("present_section")
 const future_section = document.getElementById("future_section")
 
+const header_elements = document.getElementsByClassName("header")
+const img_elements = document.getElementsByTagName("img")
+const description_elements = document.getElementsByClassName("description")
+
+
 
 function generate_random_integer(min, max) {
   let seed = Math.random();
@@ -24,9 +29,12 @@ function pull_random_indicies() {
 function load_section(element, data) {
   let img = element.getElementsByTagName('img')[0];
   let description = element.getElementsByClassName('description')[0];
-  console.log(description)
-  
+
   element.style.background = `url(${data['background_filepath']}`;
+  element.style.backgroundSize = 'cover';
+  element.style.backgroundPosition = 'center 0';
+  element.style.backgroundRepeat = 'no-repeat'
+
   img.src = data['card_filepath']
   description.textContent = data['description']
 }
@@ -45,4 +53,28 @@ async function load_tarot_data() {
   load_section(future_section, future_tarot_data)
 }
 
+function show_section(element) {
+  element.style.transition = "1s"
+  element.style.visibility = "visible"
+}
+
+function transistion_elements_array(elements_array, ms_timeout) {
+  for (let i = 0; i < elements_array.length; i++) {
+    setTimeout(show_section, ms_timeout, elements_array[i])
+  }
+}
+
+function transition_flow() {
+  setTimeout(show_section, 1000, past_section)
+  setTimeout(show_section, 2000, present_section)
+  setTimeout(show_section, 3000, future_section)
+  
+  transistion_elements_array(img_elements, 4000)
+  transistion_elements_array(header_elements, 5000)
+  transistion_elements_array(description_elements, 5000)
+}
+
 load_tarot_data()
+transition_flow()
+
+// Do screen transistions
